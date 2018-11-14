d4e739e481eb5e31e02cef26038604e8f2ff4c6f
Mao <natetymao@gmail.com>
2018-11-14 14:09:28 +0800

    v7 test

---
+++ precache-manifest.ea23589a63ad6361223cc46647c89ffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5374564bd6a5bd9e322",
+    "url": "/nameko/static/js/runtime~main.a5374564.js"
+  },
+  {
+    "revision": "c059a0e53d676aa5cb1c",
+    "url": "/nameko/static/js/main.c059a0e5.chunk.js"
+  },
+  {
+    "revision": "2d2ebc644a25af0dd7cb",
+    "url": "/nameko/static/js/1.2d2ebc64.chunk.js"
+  },
+  {
+    "revision": "c059a0e53d676aa5cb1c",
+    "url": "/nameko/static/css/main.018e90b3.chunk.css"
+  },
+  {
+    "revision": "195032ad6bb6d137f174173f0aff151e",
+    "url": "/nameko/index.html"
+  }
+];
