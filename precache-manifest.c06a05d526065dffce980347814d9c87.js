44f9096ec0a3dd9b1c63cca8bae38e186b9ba5fa
Mao <natetymao@gmail.com>
2018-12-08 17:23:59 +0800

    v16 test

---
+++ precache-manifest.c06a05d526065dffce980347814d9c87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5374564bd6a5bd9e322",
+    "url": "/nameko/static/js/runtime~main.a5374564.js"
+  },
+  {
+    "revision": "04bc2d2f11f23b8047f8",
+    "url": "/nameko/static/js/main.04bc2d2f.chunk.js"
+  },
+  {
+    "revision": "c62aba46584f3adb4c46",
+    "url": "/nameko/static/js/1.c62aba46.chunk.js"
+  },
+  {
+    "revision": "04bc2d2f11f23b8047f8",
+    "url": "/nameko/static/css/main.ffa1d020.chunk.css"
+  },
+  {
+    "revision": "676a9b52bd8ae0bcc956c79cf84bf5ad",
+    "url": "/nameko/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nameko/precache-manifest.4bae1e7480e3c96a43ea2a884dab398f.js"
+  "/nameko/precache-manifest.c06a05d526065dffce980347814d9c87.js"
 );
 
 workbox.clientsClaim();
